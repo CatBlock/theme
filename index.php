5344ab2ab390b6d8e6c4d6fa4a1edbdeac5b571f
Kieran Peckett <crazyskeggy@gmail.com>
2015-08-27 15:21:25 +0100

    Add post body

--- index.php
+++ index.php
@@ -5,10 +5,28 @@
       <div class="container">
         <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
         <div class="info_top">
-            <div class="categories">
+            <div class="post_categories">
                 Posted in <span class="category"><?php the_category(", "); ?></span>
-            </div><!-- .categories -->
+            </div><!-- .post_categories -->
+            <div class="post_author">
+                by <?php the_author_link(); ?>
+            </div><!-- .post_author -->
         </div><!-- .info_top -->
+        <div class="post_body">
+            <?php
+                if (is_archive() || is_search()){
+                    the_excerpt();
+                }
+                else{
+                    the_content("Click for full post");
+                }
+            ?>
+        </div><!-- .post_body -->
+        <div class="info_bottom">
+            <div class="post_comments">
+                <?php comments_popup_link("0 comments", "1 comment", "% comments", "comments_link", "Comments are disabled"); ?>
+            </div><!-- .post_comments -->
+        </div><!-- .info-bottom -->
       </div><!-- .container -->
     </div><!-- .post -->
   </div><!-- .content -->
